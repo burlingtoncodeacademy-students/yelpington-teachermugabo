067cffd5935b3a7b2b3543b1665631c4062cf180
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-18 12:09:16 -0400

    text change / no functional change

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Map from './components/Map';
 function App() {
   return (
     <div>
-      <h1>Yelpington: Burlington Restaurants</h1>
+      <h1>Yelpington!</h1>
       <Map />
     </div>
   );
