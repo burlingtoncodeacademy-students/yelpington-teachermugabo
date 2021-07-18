442a9513f012891334b9b5bf84dd0a03f76a7aa5
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-18 12:18:14 -0400

    add api endpoints

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use(express.static('client/build'))
+app.use(express.static('client/build'));
 
-app.listen(5000, () => console.log('Listening...'))
+// api endpoint: return all available restaurant ids
+app.get('/api', (req, res) => {
+  console.log('request on /api');
+
+  // set header - for JSON
+  res.header('type/json')
+  // send file
+  res.setHeader('Content-Type', 'application/json');
+  res.sendFile(path.resolve('api','ids.json'));
+});
+
+// api endpoint: return restaurant object
+app.get('/api/:restauId', (req, res) => {
+  const { restauId } = req.params;
+  console.log(`request on /api/${restauId}`);
+
+  // set header - for JSON
+  res.header('type/json')
+
+  // send file
+  // TODO check whether file exists first
+  const restauDir = path.resolve('api/restaurants')
+  res.sendFile(path.join(restauDir,`${restauId}.json`));
+});
+
+app.listen(5000, () => console.log('Listening...'));
