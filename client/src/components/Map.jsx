3932900de7481967c972fb837a1b82a098552455
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-18 13:43:34 -0400

    Bob - see question in Map.jsx @line 32

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,5 +1,5 @@
 import { MapContainer, Popup, Marker, TileLayer } from 'react-leaflet';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
@@ -13,6 +13,54 @@ let DefaultIcon = L.icon({
 
 const Map = () => {
   const [center, setCenter] = useState([44.47, -73.21]);
+  //const [restauIds, setRestausIds] = useState([]);
+  const [restaus, setRestau] = useState([]); // array of objects of restaurants
+
+  // populate restaurants
+  asych useEffect(() => {
+    let tempRestaus = await fetch('/api/')
+      .then((r) => r.json())
+      .then((ids) =>
+        ids.map((id) =>
+          await fetch(`/api/${id}`)
+            .then((r) => r.json())
+            .then((data) => data) 
+        )
+      );
+
+      // Question: how do I get al this into?!
+      setRestau(tempRestaus)
+  })
+
+  /*
+  // grab the ids & use those ids to
+  useEffect(() => {
+    // get Restau IDs
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('data @24', data);
+        setRestausIds(data);
+      });
+  }, []);
+  */
+
+  /*
+  // when restaudId change,
+  // get Restaurant data & push into into Restaus
+  useEffect(() => {
+    // for each Id, get a restaurant
+    if (restauIds.length) {
+      restauIds.forEach(async (restauId) => {
+        let tempRestaus;
+        let restauObj = await fetch(`/api${restauId}`).then((res) =>
+          res.json()
+        );
+        console.log(restauObj, restauObj);
+      });
+    }
+  }, [restauIds]);
+  */
 
   return (
     <div id='map-container'>
@@ -21,6 +69,10 @@ const Map = () => {
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">    OpenStreetMap</a> contributors'
         ></TileLayer>
+        {/* insert fetch here to get all restauIds, then for each restauId, fetch
+        it's information, get its latlong and create a marker for it on the map
+        :-) */}
+
         <Marker position={center} icon={DefaultIcon}>
           <Popup>Burlington is here!</Popup>
         </Marker>
