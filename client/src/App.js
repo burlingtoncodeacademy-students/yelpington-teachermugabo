a6827add49714c335e5f5dfa6b491b1b103b6bf3
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-19 01:17:33 -0400

    move state from Map to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,45 @@
+import { useState, useEffect } from 'react';
 import Map from './components/Map';
 
 function App() {
+  const [center, setCenter] = useState([44.47, -73.21]);
+  const [restaus, setRestau] = useState([]); // array of objects of restaurants
+
+  /*
+   * fetch restaurants
+   */
+  useEffect(() => {
+    const fetchRestaus = async () => {
+      // fetch restau IDs...
+      const restauIdsResp = await fetch('/api');
+      const restauIds = await restauIdsResp.json(); // works up to here
+
+      // map over IDs, fetching the rest of the restaurant objects
+      // ! wait for all async calls to resolve
+      // source: https://advancedweb.hu/how-to-use-async-functions-with-array-map-in-javascript/
+      const restausTemp = await Promise.all(
+        restauIds.map(async (id) => {
+          const restauResp = await fetch(`/api/${id}`);
+          const restauObj = await restauResp.json();
+
+          // return each restau object so the map creates full restau array
+          // console.log('restauObj', restauObj);
+          return restauObj;
+        })
+      );
+
+      // set all restaurant in state
+      // console.log('restausTemp', restausTemp);
+      setRestau(restausTemp);
+    };
+    fetchRestaus();
+  }, []);
+
   return (
     <div>
       <h1>Yelpington!</h1>
-      <Map />
+      <nav></nav>
+      <Map center={center} restaus={restaus} />
     </div>
   );
 }
--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,5 +1,4 @@
 import { MapContainer, Popup, Marker, TileLayer } from 'react-leaflet';
-import { useState, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
@@ -11,84 +10,24 @@ let DefaultIcon = L.icon({
   shadowUrl: iconShadow,
 });
 
-const Map = () => {
-  const [center, setCenter] = useState([44.47, -73.21]);
-  const [restaus, setRestau] = useState([]); // array of objects of restaurants
-
-  /*
-   * fetch restaurants
-   */
-  useEffect(() => {
-    const fetchRestaus = async () => {
-      const restauIdsResp = await fetch('/api');
-      const restauIds = await restauIdsResp.json(); // works up to here
-
-      // setRestausIds(restauIds); // set state - don't need/use this
-
-      // wait for all async calls to resolve
-      // source: https://advancedweb.hu/how-to-use-async-functions-with-array-map-in-javascript/
-      const restausTemp = await Promise.all(
-        restauIds.map(async (id) => {
-          const restauResp = await fetch(`/api/${id}`);
-          const restauObj = await restauResp.json();
-
-          console.log('restauObj', restauObj);
-          return restauObj;
-        })
-      );
-
-      console.log('restausTemp', restausTemp);
-      setRestau(restausTemp);
-    };
-    fetchRestaus();
-  }, []);
-  /* */
-
-  /*
-   * populate restaurants
-   * couldn't get this one to work
-  useEffect(() => {
-    // ? where do the 'awaits' go?
-    const fetchRestaus = async () => {
-      await fetch('/api/')
-        .then((r) => r.json())
-        .then((ids) => {
-          // get & set restaurants
-          // where to grab it from -- where to place await?
-          setRestau(
-            ids.map(
-              async (id) =>
-                await fetch(`/api/${id}`)
-                  .then((r) => r.json())
-                  .then((data) => {
-                    console.log(data) // so this is a restaurant
-                    return data;
-                  })
-            )
-          );
-        });
-    };
-    fetchRestaus();
-  }, []);
-/**/
-
+const Map = ({ center, restaus }) => {
   return (
     <div id='map-container'>
-      <MapContainer center={center} zoom={12} style={{ height: '60vh' }}>
+      <MapContainer center={center} zoom={13} style={{ height: '60vh' }}>
+        {/* select map tile + attribution  */}
         <TileLayer
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">    OpenStreetMap</a> contributors'
         ></TileLayer>
 
-        {/* start by inserting default Burlington marker  */}
-        <Marker position={center} icon={DefaultIcon}>
-          <Popup>Burlington is here!</Popup>
-        </Marker>
-
-        {/* insert fetch here to get all restauIds, then for each restauId, fetch
-        it's information, get its latlong and create a marker for it on the map
-        :-) */}
-        {restaus}
+        {/* add markers for all restaurants  */}
+        {restaus.map((restau) => (
+          <Marker key={restau.id} position={restau.latlong} icon={DefaultIcon}>
+            <Popup>
+              {restau.name} at {restau.address}
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
